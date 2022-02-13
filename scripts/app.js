ec282640a71c74f4ea62144d798446f73d3cf59e
satamame <satax@nifty.com>
2022-02-13 22:35:24 +0900

    First commit for gh-pages.

---
+++ scripts/app.js
@@ -0,0 +1,588 @@
+const appVersion = '0.0.1';
+const debug = true;
+
+// グローバル変数の定義
+let tocItems = [];                  // 見出し要素のインデックスリスト
+let tocItemIndex = 0;               // 目次で現在地としている見出しインデックス
+let scrollLocked = false;           // スクロールをロックしているかのフラグ
+let srchWord = '';
+let srchTarget = 'all';
+let srchMatches = [];
+let srchMatchIndex = 0;
+let trackingLineIndex = 0;
+let viewTopTrackingId = null;
+let lastFontSize = 4;
+let lastWritingMode = 0;
+let scUpdated = false;
+const ua = window.navigator.userAgent.toLowerCase();
+
+const fontSizeInPixel = {
+  1: '12px',
+  2: '14px',
+  3: '16px',
+  4: '18px',
+  5: '20px',
+  6: '22px',
+  7: '24px'
+};
+
+// localStorage に保存するデータ
+let dataList = [];
+let selectedDataUrl = '';
+let fontSize = 4;           // 1-7
+let writingMode = 0;        // 0: 横, 1: 縦, 2: 縦 (英数字も)
+
+window.onload = (event) => {
+  // Android OS なら main の高さを固定する (キーボード対策)
+  const main = document.getElementById("main");
+  if (ua.indexOf("android") !== -1) {
+    fixMainHeight();
+  } else {
+    // Android OS でなければ画面サイズに合わせる
+    main.style.bottom = 0;
+  }
+
+  // Android OS なら、Back ボタンの制御をする
+  if (ua.indexOf("android") !== -1) {
+    controlBackBtn();
+  }
+
+  // 文字サイズ選択メニューを初期化する
+  initFontSizeMenu();
+
+  // 横書き/縦書きを初期化する
+  initWritingMode();
+
+  // dataList を初期化して台本を描画する
+  initDataList();
+
+  // バージョンを表示する
+  document.getElementById('appVersion').innerHTML = appVersion;
+
+  // イベントハンドラを初期化する
+  initEventHandlers();
+};
+
+// サービスワーカーとリソースを再読込する関数
+function reload() {
+  if (!('serviceWorker' in navigator))
+    return;
+
+  navigator.serviceWorker.getRegistration()
+  .then(registration => {
+    if (registration.waiting != null) {
+      // registration.unregister();  // 効果が疑わしいので保留
+      alert('インストール済みの更新があります。台本ビューアを再起動してください。');
+      disableUpdateButton();
+    }
+    else {
+      registration.update()
+      .then(registration => {
+        const installingWorker = registration.installing;
+        if (installingWorker != null) {
+          installingWorker.onstatechange = e => {
+            if (e.target.state == 'installed') {
+              // registration.unregister();  // 効果が疑わしいので保留
+              alert('更新がインストールされました。台本ビューアを再起動してください。');
+              disableUpdateButton();
+            }
+          }
+        }
+        else {
+          alert('更新はありませんでした。');
+        }
+      });
+    }
+  });
+}
+
+// 更新ボタンを無効化しメッセージを表示する関数
+function disableUpdateButton() {
+  document.getElementById('updateButton').disabled = true;
+  document.getElementById('updateAlert').textContent = '台本ビューアを再起動してください。';
+}
+
+// 台本部分の高さを画面に合わせて固定する関数 (Android キーボード対策)
+function fixMainHeight() {
+  const main = document.getElementById("main");
+  let h = window.innerHeight;
+  if (h == main.width)
+    h = window.innerWidth;
+  document.getElementById("main").style.height = `${h - 48}px`;
+}
+
+// 画面回転時の処理
+function orientationChanged() {
+  // スクロールロック
+  const lastScrollLocked = scrollLocked;
+  if (!scrollLocked)
+    lockScroll();
+
+  let delay = 100;
+  // キーボードが出ていたら閉じて、その分待つ
+  if (document.activeElement.id == 'srchInput') {
+    document.activeElement.blur();
+    delay = 300;
+  }
+  // 少し待って Android なら main の高さを画面の高さに合わせる
+  setTimeout(() => {
+    if (ua.indexOf("android") !== -1)
+      fixMainHeight();
+
+    // スクロール位置を調整してスクロールをアンロック
+    jumpToLine(trackingLineIndex);
+    if (!lastScrollLocked) {
+      unlockScroll();
+      // ずれがあった時のために台本行の追跡をする
+      startTrackingViewTop();
+    }
+  }, delay);
+}
+
+// 文字サイズ選択メニューを初期化する関数
+function initFontSizeMenu() {
+  // localStorage から fontSize を取得する
+  if (localStorage.fontSize)
+    if (localStorage.fontSize in fontSizeInPixel)
+      fontSize = localStorage.fontSize;
+
+  // 文字サイズ選択メニューを初期化する
+  const fontSizeSelect = document.getElementById('fontSizeSelect');
+  while (fontSizeSelect.lastChild) {
+    fontSizeSelect.removeChild(fontSizeSelect.lastChild);
+  }
+  for (const key in fontSizeInPixel) {
+    const op = document.createElement("option");
+    op.value = key;
+    op.text = key;
+    fontSizeSelect.appendChild(op);
+    if (op.value == fontSize) {
+      op.selected = true;
+    }
+  }
+
+  // グローバル変数, localStorage, HTML に反映させる
+  changeFontSize();
+}
+
+// 文字サイズ選択メニューの値で文字サイズを反映させる関数
+function changeFontSize() {
+  // グローバル変数と localStorage を更新
+  const fontSizeSelect = document.getElementById('fontSizeSelect');
+  fontSize = parseInt(fontSizeSelect.value);
+  localStorage.fontSize = fontSize;
+
+  // HTML に反映させる
+  const main = document.getElementById('main');
+  main.style.fontSize = fontSizeInPixel[fontSize];
+}
+
+// 横書き/縦書きを初期化する関数
+function initWritingMode() {
+  // localStorage から writingMode を取得する
+  if (localStorage.writingMode)
+    writingMode = localStorage.writingMode;
+
+  // 向きメニューを更新する
+  const writingModeSelect = document.getElementById('writingModeSelect');
+  for (const option of writingModeSelect.options) {
+    option.selected = (parseInt(option.value) == writingMode);
+  }
+
+  // HTML に反映させる
+  applyWritingMode();
+}
+
+// 向きメニューの値で横書き/縦書きを反映させる関数
+function changeWritingMode() {
+  // グローバル変数と localStorage を更新
+  const writingModeSelect = document.getElementById('writingModeSelect');
+  writingMode = parseInt(writingModeSelect.value);
+  localStorage.writingMode = writingMode;
+
+  // HTML に反映させる
+  applyWritingMode();
+}
+
+// 横書き/縦書きを HTML に反映させる関数
+function applyWritingMode() {
+  const main = document.getElementById('main');
+  if (writingMode > 0) {
+    main.classList.add('vertical');
+    if (writingMode > 1)
+      main.classList.add('upright');
+    else
+      main.classList.remove('upright');
+  } else {
+    main.classList.remove('vertical');
+    main.classList.remove('upright');
+  }
+}
+
+// dataList を初期化して台本を描画する関数
+function initDataList() {
+  // localStorage から dataList を取得する
+  const dataJson = localStorage.dataList;
+  if (dataJson)
+    dataList = JSON.parse(dataJson);
+
+  // localStorage から選択中の台本の URL を取得する
+  const url = localStorage.selectedDataUrl;
+  if (url)
+    selectedDataUrl = url;
+
+  // dataList と selectedDataUrl から台本選択メニューを更新する
+  updateScMenu();
+  // dataList と selectedDataUrl の状態を HTML に反映させる
+  scLoad();
+}
+
+// dataList と selectedDataUrl から台本選択メニューを更新する関数
+function updateScMenu() {
+  // 台本選択メニューに要素をセットする
+  const scSelect = document.getElementById('scSelect');
+  while (scSelect.lastChild) {
+    scSelect.removeChild(scSelect.lastChild);
+  }
+  for (const dataItem of dataList) {
+    const op = document.createElement("option");
+    op.value = dataItem.url;
+    op.text = dataItem.title;
+    scSelect.appendChild(op);
+    if (op.value == selectedDataUrl) {
+      op.selected = true;
+    }
+  }
+}
+
+// 台本のスクロールをロックする関数
+function lockScroll() {
+  scrollLocked = true;
+  // 台本行の追跡中なら中断する
+  if (viewTopTrackingId != null)
+    window.clearInterval(viewTopTrackingId);
+  const main = document.getElementById('main');
+  main.classList.add('scroll-disabled');
+}
+
+// 台本のスクロールロックを解除する関数
+function unlockScroll() {
+  const main = document.getElementById('main');
+  main.classList.remove('scroll-disabled');
+  scrollLocked = false;
+}
+
+// 目次を表示する関数
+function showToc() {
+  lockScroll();
+  document.getElementById("toc").style.visibility = "visible";
+  showUpCurrentTocItem();
+
+  if (document.getElementById("srchHeader").style.visibility == "visible") {
+    // 検索ヘッダが表示中ならバックボタン対応のための履歴を差し替え
+    window.history.replaceState({ activity: 'toc' }, '');
+  } else {
+    // さもなくばバックボタン対応のため履歴を追加
+    window.history.pushState({ activity: 'toc' }, '');
+  }
+}
+
+// 現在地の目次項目を目立たせる関数
+function showUpCurrentTocItem() {
+  // 強調解除
+  const tocList = document.getElementById('tocList');
+  tocList.children[tocItemIndex].classList.remove('current');
+
+  // 現在地を検出
+  tocItemIndex = detectTocItemIndex();
+
+  // 現在地の目次項目を強調する
+  const currentItem = tocList.children[tocItemIndex];
+  currentItem.classList.add('current');
+  currentItem.scrollIntoView({block: 'center'});
+}
+
+// 目次を閉じる関数
+function hideToc() {
+  document.getElementById("toc").style.visibility = "hidden";
+  unlockScroll();
+
+  if (document.getElementById("srchHeader").style.visibility != "visible") {
+    // 検索ヘッダが表示中でなければ、バックボタン対応のため追加した履歴を削除
+    window.history.back();
+  }
+}
+
+// 検索を開始する関数
+function startSearching() {
+  srchWord = document.getElementById("srchInput").value;
+  if (!srchWord)
+    return;
+
+  // 検索ヘッダを表示する
+  document.getElementById("srchWord").innerText = srchWord;
+  document.getElementById("normalHeader").style.visibility = "hidden";
+  document.getElementById("srchHeader").style.visibility = "visible";
+
+  if (document.getElementById("toc").style.visibility == "visible") {
+    // 目次が表示中なら閉じて、バックボタン対応のための履歴を差し替え
+    document.getElementById("toc").style.visibility = "hidden";
+    unlockScroll();
+    window.history.replaceState({ activity: 'search' }, '');
+  } else {
+    // さもなくばバックボタン対応のため履歴を追加
+    window.history.pushState({ activity: 'search' }, '');
+  }
+
+  // 検索実行
+  srchTarget = document.getElementById("srchTargetSelect").value;
+  srchMatches = listSrchMatches(srchWord, srchTarget);
+  srchMatchIndex = indexOfSrchMatchInView();
+  gotoSrchMatch(srchMatchIndex);
+}
+
+// エンターキーが押されたことによって検索をする関数
+function searchByEnterKey(e) {
+  e.currentTarget.blur();
+  startSearching();
+  e.preventDefault();
+}
+
+// 注目する検索結果をひとつ前にする関数
+function srchPrev() {
+  if (srchMatchIndex <= 0)
+    return;
+  if (srchMatchIndex <= 1)
+    gotoSrchMatch(srchMatches.length);
+  else {
+    gotoSrchMatch(srchMatchIndex - 1);
+  }
+}
+
+// 注目する検索結果をひとつ後にする関数
+function srchNext() {
+  if (srchMatchIndex <= 0)
+    return;
+  if (srchMatchIndex >= srchMatches.length)
+    gotoSrchMatch(1);
+  else {
+    gotoSrchMatch(srchMatchIndex + 1);
+  }
+}
+
+// 検索ヘッダを閉じる関数
+function stopSearching() {
+  // 検索結果の強調表示をクリア
+  clearSrchMatches();
+
+  document.getElementById("normalHeader").style.visibility = "visible";
+  document.getElementById("srchHeader").style.visibility = "hidden";
+
+  // バックボタン対応のため追加した履歴を削除
+  window.history.back();
+}
+
+// 設定を表示する関数
+function showSetting() {
+  lockScroll();
+  scUpdated = false;              // 台本が変わったフラグを初期化
+  lastFontSize = fontSize;        // フォントサイズを憶えておく
+  lastWritingMode = writingMode;  // 横書き/縦書き を憶えておく
+  document.getElementById("setting").style.visibility = "visible";
+  // バックボタン対応のため履歴を追加
+  window.history.pushState({ activity: 'setting' }, '');
+}
+
+// 設定を閉じる関数
+function hideSetting() {
+  document.getElementById("setting").style.visibility = "hidden";
+
+  if (debug) console.log(`*** scUpdated: ${scUpdated}`);
+
+  // 台本やフォントサイズや 横書き/縦書き が変わったならスクロールを調整
+  if (scUpdated || fontSize != lastFontSize || writingMode != lastWritingMode) {
+    jumpToLine(trackingLineIndex);
+    if (debug) console.log('*** Scroll restored.');
+    unlockScroll();
+    // ずれがあった時のために台本行の追跡をする
+    startTrackingViewTop();
+  } else {
+    unlockScroll();
+  }
+  // バックボタン対応のため追加した履歴を削除
+  window.history.back();
+}
+
+// dataList と selectedDataUrl の状態を HTML に反映させる関数
+function scLoad() {
+  // 台本が変わったフラグを立てる
+  scUpdated = true;
+  // 台本が反映されたかの判定用にタイトルを初期化
+  let title = '';
+
+  // dataList から選択中の台本データを抽出
+  const selected = dataList.filter(item => {
+    return (item.url == selectedDataUrl);
+  });
+
+  if (selected.length >= 1) {
+    // PSc データを main 要素に反映させる
+    loadPSc(selected[0].psc);
+
+    // タイトルを設定する
+    title = selected[0].title;
+  }
+
+  // タイトルが設定されていなければヘルプを表示
+  if (!title) {
+    title = '台本ビューア';
+    fetch('psc/help.json').then(response => {
+      if (response.ok) {
+        response.json().then(data => {
+          loadPSc(data.psc);
+          title = data.title;
+        });
+      } else {
+        clearPSc();
+      }
+    })
+    .catch(error => {
+      clearPSc();
+    });
+  }
+
+  // タイトルをヘッダに反映させる
+  const headerTitle = document.getElementById('headerTitle');
+  headerTitle.innerHTML = title;
+}
+
+// 台本選択メニューで選択された台本を読み込む関数 (変更があった時)
+function scLoadFromMenu() {
+  // 選択中の URL を取得
+  const scSelect = document.getElementById('scSelect');
+  if (scSelect.selectedOptions.length < 1)
+    return;
+
+  const url = scSelect.value;
+  if (url != selectedDataUrl) {
+    // この台本を選択中にする
+    selectedDataUrl = url;
+    localStorage.selectedDataUrl = selectedDataUrl;
+
+    // dataList と selectedDataUrl の状態を HTML に反映させる
+    scLoad();
+  }
+}
+
+// 台本データを追加する関数
+function scAdd() {
+  const url = prompt('台本データの URL');
+  if (!url)
+    return;
+
+  fetch(url).then(response => {
+    if (response.ok) {
+      response.json().then(data => {
+        scAddToList(data, url);
+      });
+    } else {
+      alert('台本データを取得できませんでした。');
+    }
+  })
+  .catch(error => {
+    alert('台本データを取得できませんでした。');
+  });
+}
+
+// 台本データをリストに追加する関数
+function scAddToList(data, url) {
+  // すでに同じ URL があれば、古い方を削除する
+  const dupe = dataList.find(value => value.url == url);
+  if (dupe) {
+    dataList = dataList.filter(item => {
+      return (item.url != url);
+    });
+    alert('同じ URL の古いエントリを削除しました。');
+  }
+
+  // 台本データに URL とタイトルを埋め込む
+  data.url = url;
+  data.title = data.psc.title;
+
+  // 台本データを dataList に加え、localStorage に保存する
+  dataList.push(data);
+  localStorage.dataList = JSON.stringify(dataList);
+
+  // この台本を選択中にする
+  selectedDataUrl = url
+  localStorage.selectedDataUrl = selectedDataUrl;
+
+  // 台本選択メニューを更新する
+  updateScMenu();
+
+  // dataList と selectedDataUrl の状態を HTML に反映させる
+  scLoad();
+}
+
+// 台本データを再取得する関数
+function scReload() {
+  const scSelect = document.getElementById('scSelect');
+  if (scSelect.selectedOptions.length < 1)
+    return;
+
+  const selected = scSelect.selectedOptions[0];
+  const doReload = confirm(`「${selected.text}」を再取得します。`);
+  if (!doReload) return;
+
+  // dataList から選択中の台本データを抽出
+  const url = selected.value;
+  const selectedData = dataList.filter(item => {return (item.url == url);});
+  if (selectedData.length < 1)
+    return;
+
+  fetch(url).then(response => {
+    if (response.ok) {
+      response.json().then(data => {
+        // データを上書きする
+        selectedData[0].title = data.psc.title;
+        selectedData[0].psc = data.psc;
+        localStorage.dataList = JSON.stringify(dataList);
+        // この台本を選択中にする
+        selectedDataUrl = url;
+        localStorage.selectedDataUrl = selectedDataUrl;
+        // dataList と selectedDataUrl の状態を HTML に反映させる
+        scLoad();
+      });
+    } else {
+      alert('台本データを取得できませんでした。');
+    }
+  })
+  .catch(error => {
+    alert('台本データを取得できませんでした。');
+  });
+}
+
+// 台本データを削除する関数
+function scDelete() {
+  const scSelect = document.getElementById('scSelect');
+  if (scSelect.selectedOptions.length < 1)
+    return;
+
+  const selected = scSelect.selectedOptions[0];
+  const doDelete = confirm(`「${selected.text}」を削除します。`);
+  if (!doDelete) return;
+
+  // 選択中の URL の台本を dataList から削除
+  dataList = dataList.filter(item => {
+    return (item.url != selected.value);
+  });
+
+  // localStorage に反映させる
+  localStorage.dataList = JSON.stringify(dataList);
+
+  // dataList と selectedDataUrl から台本選択メニューを更新する
+  updateScMenu();
+
+  // 台本選択メニューで選択された台本を読み込む
+  scLoadFromMenu();
+}
---
+++ scripts/psc.js
@@ -0,0 +1,410 @@
+const map2class = {
+  TITLE: 'title',
+  AUTHOR: 'author',
+  CHARSHEADLINE: 'chars-headline',
+  CHARACTER: 'character',
+  H1: 'headline-1',
+  H2: 'headline-2',
+  H3: 'headline-3',
+  DIRECTION: 'direction',
+  DIALOGUE: 'dialogue',
+  ENDMARK: 'endmark',
+  COMMENT: 'comment',
+  EMPTY: 'empty'
+};
+
+// main 要素内の台本をクリアする関数
+function clearPSc() {
+  if (debug) console.log('*** clearPSc called.');
+
+  // 台本行の追跡を中断し、現在地を初期化する
+  if (viewTopTrackingId != null)
+    window.clearInterval(viewTopTrackingId);
+  trackingLineIndex = 0;
+
+  // main 要素の子孫を削除する
+  const main = document.getElementById('main')
+  while (main.lastChild) {
+    main.removeChild(main.lastChild);
+  }
+}
+
+// tocList 内の項目をクリアする関数
+function clearTocList() {
+  // tocList の子孫を削除する
+  const tocList = document.getElementById('tocList')
+  while (tocList.lastChild) {
+    tocList.removeChild(tocList.lastChild);
+  }
+  // 目次項目リストを初期化する
+  tocItems = [];
+}
+
+// PSc データを main 要素に反映させる関数
+function loadPSc(pscData) {
+  // main 要素内の台本をクリアする
+  clearPSc();
+
+  // 目次をクリアする
+  clearTocList();
+
+  // HTML をエスケープする関数
+  function e(str) {
+    escaped = str.replace(/&/g, '&amp;');
+    escaped = escaped.replace(/</g, '&lt;');
+    escaped = escaped.replace(/>/g, '&gt;');
+    return escaped;
+  }
+
+  // 空白文字をエスケープする関数
+  function eSpc(str) {
+    escaped = str.replace(/\r?\n/g, '<br>');
+    escaped = escaped.replace(/\s/g, '&nbsp;');
+    return escaped;
+  }
+
+  const tocList = document.getElementById('tocList')
+
+  for (const [index, pscLine] of pscData.lines.entries()) {
+    // 台本行要素
+    const lineElm = document.createElement('div');
+    lineElm.classList.add(map2class[pscLine.type]);
+
+    if (pscLine.type == 'CHARACTER') {
+      lineElm.innerHTML = `<p>${e(pscLine.name)}</p>`;
+      if (pscLine.text)
+        lineElm.innerHTML += `<p>${eSpc(e(pscLine.text))}</p>`;
+    } else if (pscLine.type == 'DIALOGUE') {
+      lineElm.innerHTML = `<p>${e(pscLine.name)}</p>`;
+      lineElm.innerHTML += `<p>${eSpc(e(pscLine.text))}</p>`;
+    } else {
+      lineElm.innerHTML = `<p>${eSpc(e(pscLine.text))}</p>`;
+    }
+
+    // 見出しなら目次に追加する
+    if (['TITLE', 'CHARSHEADLINE', 'H1', 'H2', 'H3'].includes(pscLine.type)) {
+      const tocItem = document.createElement('li');
+      tocItem.innerHTML = pscLine.text;
+      tocItem.setAttribute('onclick', `jumpToTocItem(${tocItems.length});`);
+      tocList.appendChild(tocItem);
+
+      // 目次項目リストにも番号を追加する
+      tocItems.push(index);
+    }
+
+    // 台本行要素を main の子供に追加する
+    document.getElementById('main').appendChild(lineElm);
+  }
+}
+
+// 行番号を指定してジャンプする関数
+function jumpToLine(lineNum) {
+  let x = 0;
+  let y = 0;
+
+  if (lineNum > 0) {
+    // スクロール先の要素を決める
+    const main = document.getElementById('main');
+    let lineElm = main.children[lineNum];
+
+    // 子要素 (p とか) があれば、そっちを使う
+    if (lineElm.childElementCount > 0) {
+      lineElm = lineElm.children[0]
+    }
+
+    // スクロール先の座標を求める
+    if (writingMode < 1) {
+      // 横書きの場合
+      const marginTop = window.getComputedStyle(lineElm).getPropertyValue('margin-top');
+      y = lineElm.offsetTop - parseFloat(marginTop);
+      if (debug) console.log(`*** y: ${y}`);
+    } else {
+      // 縦書きの場合
+      const marginRight = window.getComputedStyle(lineElm).getPropertyValue('margin-right');
+      const viewWidth = document.getElementById('main').offsetWidth;
+      x = lineElm.offsetLeft + lineElm.offsetWidth + parseFloat(marginRight) - viewWidth;
+    }
+  }
+  main.scrollTo(x, y);
+}
+
+// インデックスを指定して目次項目にジャンプする関数
+function jumpToTocItem(i) {
+  hideToc();
+
+  // 台本行の追跡を中断し、現在地を更新する
+  if (viewTopTrackingId != null)
+    window.clearInterval(viewTopTrackingId);
+  lineNum = tocItems[i];
+  trackingLineIndex = lineNum;
+
+  // その行へジャンプする
+  jumpToLine(lineNum);
+}
+
+// 現在地の目次項目が何番目かを返す関数
+function detectTocItemIndex() {
+  if (writingMode < 1)
+    return detectTocItemIndexH();
+  else
+    return detectTocItemIndexV();
+}
+
+// 現在地の目次項目が何番目かを返す関数 (横書きの場合)
+function detectTocItemIndexH() {
+  const main = document.getElementById('main');
+  const scrollTop = main.scrollTop;
+
+  // 下まで走査した時の初期値を、最後の目次項目にする
+  let tocItemIndex = tocItems.length -1;
+
+  // 画面の上端以下にある目次項目を探す
+  for (let i = 0; i < tocItems.length; i++) {
+    const lineNum = tocItems[i];
+    const lineElm = main.children[lineNum];
+
+    if (lineElm.offsetTop - scrollTop >= 0) {
+      tocItemIndex = i;
+      break;
+    }
+  }
+
+  // 見つかった目次項目の行番号
+  const lineNum = tocItems[tocItemIndex];
+
+  if (lineNum > 0) {
+    // 見つかった目次項目の、ひとつ上の台本行要素
+    const prevElm = main.children[lineNum - 1];
+    const prevElmTop = prevElm.offsetTop - scrollTop;
+
+    // ひとつ上の要素が画面内に収まっていたら、ひとつ前の目次要素にする
+    if (prevElmTop >= 0)
+      tocItemIndex -= 1;
+  }
+
+  return tocItemIndex;
+}
+
+// 現在地の目次項目が何番目かを返す関数 (縦書きの場合)
+function detectTocItemIndexV() {
+  const main = document.getElementById('main');
+  const scrollRight = main.scrollLeft + main.offsetWidth;
+
+  // 目次項目が見つからなかった場合の値を、最後の目次項目にする
+  let tocItemIndex = tocItems.length -1;
+
+  // 画面の右端またはそれより左にある目次項目を探す
+  for (let i = 0; i < tocItems.length; i++) {
+    const lineNum = tocItems[i];
+    const lineElm = main.children[lineNum];
+
+    if (lineElm.offsetLeft + lineElm.offsetWidth - scrollRight <= 0) {
+      tocItemIndex = i;
+      break;
+    }
+  }
+
+  // 見つかった目次項目の行番号
+  const lineNum = tocItems[tocItemIndex];
+
+  if (lineNum > 0) {
+    // 見つかった目次項目の、ひとつ右の台本行要素
+    const prevElm = main.children[lineNum - 1];
+    const prevElmRight = prevElm.offsetLeft + prevElm.offsetWidth - scrollRight;
+
+    // ひとつ右の要素が画面内に収まっていたら、ひとつ前の目次要素にする
+    if (prevElmRight <= 0)
+      tocItemIndex -= 1;
+  }
+
+  return tocItemIndex;
+}
+
+// 検索結果の強調表示をクリアする関数
+function clearSrchMatches() {
+  const main = document.getElementById('main');
+
+  // 強調表示された span を持つ p 要素を取得する
+  let xpath = "//p[span[contains(@class, 'matched')]]";
+  let matchedElements = document.evaluate(
+    xpath, main, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+
+  // 各 p 要素について、子の強調を解除する
+  for (var i = 0; i < matchedElements.snapshotLength; i++) {
+    const el = matchedElements.snapshotItem(i);
+    let htmlStr = '';
+    // 子ノードに分けて、強調された span ならテキストにする
+    for (const node of el.childNodes) {
+      if (node.nodeName.toLowerCase() == 'span' && node.classList.contains('matched'))
+        htmlStr += node.textContent;
+      else if (node.nodeType == Node.TEXT_NODE)
+        htmlStr += node.textContent;
+      else
+        htmlStr += node.outerHTML;
+    }
+    el.innerHTML = htmlStr;
+  }
+}
+
+// 検索に一致した文字列を HTML 要素にして配列に入れて返す関数
+function listSrchMatches(srchWord, target) {
+  /*
+   srchWord: 検索ワード
+   target:   対象 (all, name, line, direction)
+  */
+
+  clearSrchMatches();
+
+  const main = document.getElementById('main');
+  let xpath = '';
+  switch (target) {
+    case 'name':
+      xpath = "div[@class='dialogue']/p[1]";
+      break;
+    case 'line':
+      xpath = "div[@class='dialogue']/p[position()>1]";
+      break;
+    case 'direction':
+      xpath = "div[@class='direction']/p";
+      break;
+    default:
+      xpath = 'div/p';
+  }
+
+  // 検索対象となる p 要素を取得する
+  let srchTargetElements = document.evaluate(
+    xpath, main, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+
+  // 正規表現オブジェクト
+  let re = new RegExp(srchWord, 'gi');
+
+  for (var i = 0; i < srchTargetElements.snapshotLength; i++) {
+    const el = srchTargetElements.snapshotItem(i);
+    let htmlStr = '';
+    // 子ノードに分けて、テキストノードなら処理する
+    for (const node of el.childNodes) {
+      if (node.nodeType == Node.TEXT_NODE)
+        // テキストノードだった場合はマッチした部分文字列を強調表示する
+        htmlStr += node.textContent.replace(re, '<span class="matched">$&</span>');
+      else
+        htmlStr += node.outerHTML;
+    }
+    el.innerHTML = htmlStr;
+  }
+
+  // 強調表示した部分を取り出して配列にする
+  const foundElements = [];
+  xpath = "//span[@class='matched']";
+  let matchedElements = document.evaluate(
+    xpath, main, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+  for ( var j=0 ; j < matchedElements.snapshotLength; j++ ) {
+    foundElements.push(matchedElements.snapshotItem(j));
+  }
+
+  return foundElements;
+}
+
+// 画面上端 (右端) 以降にある検索結果のインデックスを返す
+function indexOfSrchMatchInView() {
+  if (srchMatches.length <= 0)
+    return 0;
+
+  const main = document.getElementById('main');
+  if (writingMode < 1) {
+    // 横書きの場合
+    for (var i = 0; i < srchMatches.length; i++) {
+      if (srchMatches[i].offsetTop >= main.scrollTop)
+        return i + 1;
+    }
+    return 1;
+  } else {
+    // 縦書きの場合
+    const scrollRight = main.scrollLeft + main.offsetWidth;
+    for (var i = 0; i < srchMatches.length; i++) {
+      if (srchMatches[i].offsetLeft + srchMatches[i].offsetWidth <= scrollRight)
+        return i + 1;
+    }
+    return 1;
+  }
+}
+
+// 注目する検索結果を index 番目の検索結果にする
+function gotoSrchMatch(index) {
+  if (srchMatchIndex > 0) {
+    const current = srchMatches[srchMatchIndex - 1];
+    current.classList.remove('current');
+  }
+  if (index > 0) {
+    const newEl = srchMatches[index - 1];
+    newEl.classList.add('current');
+    newEl.scrollIntoView({block: "center"});
+  }
+  srchMatchIndex = index;
+  document.getElementById("srchCount").textContent = `${srchMatchIndex}/${srchMatches.length}`;
+}
+
+// 画面上端 (または右端) にある台本行の追跡を開始する
+function startTrackingViewTop() {
+  if (debug) console.log('*** startTrackingViewTop called.');
+
+  // 追跡中なら現在の追跡を中断する
+  if (viewTopTrackingId != null)
+    window.clearInterval(viewTopTrackingId);
+
+  // writingMode に応じて追跡用の関数を呼び続ける
+  if (writingMode < 1)
+    viewTopTrackingId = window.setInterval(trackViewTopH, 10);
+  else
+    viewTopTrackingId = window.setInterval(trackViewTopV, 10);
+}
+
+// 画面の上端にある台本行を追跡する (横書きの場合)
+function trackViewTopH() {
+  const main = document.getElementById('main');
+  const curElm = main.children[trackingLineIndex];
+
+  // 注目している台本行の上端が画面上端より上にあるなら、ひとつ下を注目する
+  if (trackingLineIndex < main.childElementCount - 1) {
+    if (curElm.offsetTop < main.scrollTop) {
+      trackingLineIndex++;
+      return;
+    }
+  }
+  // 注目している台本行のひとつ前の台本行の上端が画面上端以下にあるなら、それを注目する
+  if (trackingLineIndex > 0) {
+    if (main.children[trackingLineIndex -1].offsetTop >= main.scrollTop) {
+      trackingLineIndex--;
+      return;
+    }
+  }
+  // 注目している台本行が画面上端に位置しているので追跡を中断する
+  window.clearInterval(viewTopTrackingId);
+  viewTopTrackingId = null;
+  if (debug) console.log(`*** trackingLineIndex: ${trackingLineIndex}`);
+}
+
+// 画面の右端にある台本行を追跡する (縦書きの場合)
+function trackViewTopV() {
+  const main = document.getElementById('main');
+  const curElm = main.children[trackingLineIndex];
+
+  // 注目している台本行の右端が画面上右端より右にあるなら、ひとつ左を注目する
+  if (trackingLineIndex < main.childElementCount - 1) {
+    if (curElm.offsetLeft + curElm.offsetWidth > main.scrollLeft + main.offsetWidth) {
+      trackingLineIndex++;
+      return;
+    }
+  }
+  // 注目している台本行のひとつ前の台本行の右端が画面右端またはそれより左にあるなら、それを注目する
+  if (trackingLineIndex > 0) {
+    const prevElm = main.children[trackingLineIndex - 1];
+    if (prevElm.offsetLeft + prevElm.offsetWidth <= main.scrollLeft + main.offsetWidth) {
+      trackingLineIndex--;
+      return;
+    }
+  }
+  // 注目している台本行が画面右端に位置しているので追跡を中断する
+  window.clearInterval(viewTopTrackingId);
+  viewTopTrackingId = null;
+  if (debug) console.log(`*** trackingLineIndex: ${trackingLineIndex}`);
+}
