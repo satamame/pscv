2c3c1a284154c8bff26972f44bbb08e8ace9fb19
satamame <satax@nifty.com>
2022-09-12 01:05:03 +0900

    Header component added.

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,54 @@
 <script lang="ts">
-  import Counter from "./lib/Counter.svelte";
+  import Header from "./components/Header.svelte";
 </script>
 
-<header>
-  aaa
-</header>
+<Header />
 <main>
   <div>bbb</div>
+  <div>bbb</div>
+  <div>bbb</div>
+  <div>bbb</div>
+  <div>ccccc</div>
+  <div>bbb</div>
+  <div>bbb</div>
+  <div>bbb</div>
+  <div>bbb</div>
+  <div>ccccc</div>
+  <div>bbb</div>
+  <div>bbb</div>
+  <div>bbb</div>
+  <div>bbb</div>
+  <div>ccccc</div>
+  <div>bbb</div>
+  <div>bbb</div>
+  <div>bbb</div>
+  <div>bbb</div>
+  <div>ccccc</div>
+  <div>bbb</div>
+  <div>bbb</div>
+  <div>bbb</div>
+  <div>bbb</div>
+  <div>ccccc</div>
+  <div>bbb</div>
+  <div>bbb</div>
+  <div>bbb</div>
+  <div>bbb</div>
+  <div>ccccc</div>
+  <div>bbb</div>
+  <div>bbb</div>
+  <div>bbb</div>
+  <div>bbb</div>
+  <div>ccccc</div>
 </main>
 
 <style>
-header {
-  position: fixed;
-  top: 0;
-  width: 100%;
-  height: 48px;
-  background: #EEB63D;
-  text-align: center;
-}
-
-main {
-  position: fixed;
-  top: 48px;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  overflow: auto;
-  padding: 2px 12px 12px 18px;
-}
+  main {
+    position: fixed;
+    top: 48px;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    overflow: auto;
+    padding: 2px 12px 12px 18px;
+  }
 </style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+</script>
+
+<header>
+  <button class="left-button">
+    <img alt="目次" src="public/format_list_bulleted_black_24dp.svg" />
+  </button>
+  <h1>タイトル</h1>
+  <button class="right-button">
+    <img alt="設定" src="public/settings_black_24dp.svg" />
+  </button>
+</header>
+
+<style>
+  header {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    height: 48px;
+    background: #EEB63D;
+    text-align: center;
+  }
+  header h1 {
+    color: white;
+    font-size: 24px;
+    margin: 12px 50px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  button {
+    width: 28px;
+    height: 28px;
+    border-width: 0;
+    border-radius: 0;
+    padding: 0;
+    background: transparent;
+    cursor: pointer;
+  }
+  button img {
+    width: 100%;
+    height: 100%;
+  }
+  button.left-button {
+    position: fixed;
+    top: 10px;
+    left: 16px;
+  }
+  button.right-button {
+    position: fixed;
+    top: 10px;
+    right: 16px;
+  }
+</style>
