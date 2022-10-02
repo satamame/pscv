ace08b8c2f68937e7ecc064bc446c77426ed9cb5
satamame <satax@nifty.com>
2022-10-02 12:43:40 +0900

    Minimal PWA requirements (incl. icons)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,48 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
+
+const pwaOptions = {
+  strategies: 'generateSW' as 'generateSW', // default (unnecessary)
+  registerType: 'prompt' as 'prompt',       // default (unnecessary)
+  manifest: {
+    name: '台本ビューア',
+    short_name: 'PSCV',
+    description: '台本データ (JSON) を読むためのビューアです。',
+    icons: [
+      {
+        src: 'app_icon/icon-192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: 'app_icon/icon-512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      },
+      {
+        src: 'app_icon/icon-512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable'
+      }
+    ],
+    start_url: 'index.html',
+    display: 'standalone',        // default (unnecessary)
+    background_color: '#BD913F',
+    theme_color: '#916028',
+    lang: 'ja',
+  },
+  devOptions: {
+    enabled: true,
+  },
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/pscv/',
-  plugins: [svelte()]
+  plugins: [
+    svelte(),
+    VitePWA(pwaOptions),
+  ],
 })
