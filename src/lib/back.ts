2a25f0b1d73238762f49e683cea26edba0dd6f14
satamame <satax@nifty.com>
2022-11-01 23:14:02 +0900

    Clear history later than adding popstate handler.

--- src/lib/back.ts
+++ src/lib/back.ts
@@ -13,11 +13,6 @@ export function keepBackable() {
 
 /** Prepare back button handler */
 export function initBackHandler(getBackFunc: () => BackFunc): void {
-  // Clear backHook history just in case
-  if (history.state?.backHook) {
-    history.back()
-  }
-
   // When back button pressed, call appropriate function
   window.addEventListener('popstate', function (event) {
     const backFunc = getBackFunc()
@@ -32,4 +27,8 @@ export function initBackHandler(getBackFunc: () => BackFunc): void {
       backFunc.about()
     }
   })
+  // Clear backHook history just in case
+  if (history.state?.backHook) {
+    history.back()
+  }
 }
